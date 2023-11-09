88291f7d9396d49f12d4f60a7d7dd88ae213cc23
unknown <rubikonspace@ukr.net>
2023-11-09 12:44:27 +0200

    45

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,24 +3,17 @@ import { Button, ContactItem } from './ContactList.styled';
 
 // import { deleteContact } from 'components/redux/contactSlice';
 import { deleteContact } from 'components/redux/operations';
-import { selectContacts, selectFilter } from 'components/redux/selectors';
+import {
+  selectContacts,
+  selectFilter,
+  selectVisibleContacts,
+} from 'components/redux/selectors';
 import toast from 'react-hot-toast';
 
 const ContactList = () => {
   const dispatch = useDispatch();
 
-  const { items } = useSelector(selectContacts);
-
-  const filter = useSelector(selectFilter);
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return items.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  const filteredContacts = getVisibleContacts();
+  const filteredContacts = useSelector(selectVisibleContacts);
 
   const handleDeleteContact = id => {
     dispatch(deleteContact(id));
--- src/components/redux/selectors.js
+++ src/components/redux/selectors.js
@@ -5,7 +5,14 @@ export const selectFilter = state => state.filter.filter;
 // export const getIsLoading = state => state.contacts.isLoading;
 // export const getError = state => state.contacts.error;
 
-const selectVisibleContacts = state => {
+export const selectVisibleContacts = state => {
   const contacts = state.contacts.items;
-  const filterValue = state.filter;
+
+  const filterValue = state.filter.filter;
+
+  const normalizedFilter = filterValue.toLowerCase();
+
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
 };
